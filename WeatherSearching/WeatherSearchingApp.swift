52e3eb80a053a986fc4bd2ee8028e3d5620fa96e
Andres Chan <ccheifany@gmail.com>
2021-06-05 02:33:02 +0800

    create xcode project and gitignore

---
+++ WeatherSearching/WeatherSearchingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSearchingApp.swift
+//  WeatherSearching
+//
+//  Created by Andres Chan on 5/6/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSearchingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
